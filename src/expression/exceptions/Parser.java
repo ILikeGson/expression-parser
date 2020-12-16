4cd2accc9de6baab0d3d0714de7abd7de7277ffb
ZhenyaShvyrkov <67383878+ZhenyaShvyrkov@users.noreply.github.com>
2020-12-16 02:46:38 -0600

    Update Parser.java

--- src/expression/exceptions/Parser.java
+++ src/expression/exceptions/Parser.java
@@ -2,9 +2,6 @@ package expression.exceptions;
 
 import expression.TripleExpression;
 
-/**
- * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
- */
 public interface Parser {
-    TripleExpression parse(String expression) throws /* Change me */ Exception;
+    TripleExpression parse(String expression) throws Exception;
 }
